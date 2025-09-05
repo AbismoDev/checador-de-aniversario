cda749fa39601eef846a80d87fd0c2162aca2801
AbismoDev <contato.abismodev@gmail.com>
2025-09-05 11:11:05 -0300

    refactor: refatoramos nosso código

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,17 +4,15 @@ document.addEventListener("DOMContentLoaded", () => {
     form.addEventListener("submit", (e) => {
         e.preventDefault();
 
+        let msg;
+
         const dataInput = document.querySelector("#data--input");
         const res = document.querySelector("#res");
 
         if(dataInput.value === "" || dataInput.value === null || dataInput.value === undefined) {
             // erro
-            console.log("Insira uma data de nascimento antes para fazermos a checagem!");
-            res.innerHTML = `
-                <div class="container--mensagem erro">
-                    <p>Insira uma data de nascimento antes para fazermos a checagem!</p>
-                </div>
-            `
+            msg = `Insira uma data de nascimento antes para fazermos a checagem!`;
+            res.innerHTML = exibirMensagem(msg, "erro");
             return;
         }
 
@@ -26,29 +24,35 @@ document.addEventListener("DOMContentLoaded", () => {
 
         if(dataAtual.getDate() === dataNascimento.getDate() && dataAtual.getMonth() === dataNascimento.getMonth()) {
             // Aqui faremos a mensagem de parabens.            
+            msg = `🎉 Parabéns, hoje é seu aniversario! 🎉`;
+            res.innerHTML = exibirMensagem(msg, "sucesso");
+            
             const musicaParabens = new Audio("assets/audio/musica--parabens.mp3");
-            res.innerHTML = `
-                <div class="container--mensagem sucesso">
-                    <p>🎉 Parabéns, hoje é seu aniversario! 🎉</p>
-                </<div>
-            `
             musicaParabens.volume = 0.3;
             musicaParabens.play();
         }
         else {
             // Aqui faremos o retorno de quantos dias faltam para o proximo aniversario.
-            const proximoAniversario = new Date((dataAtual.getFullYear() + 1), dataNascimento.getMonth(), dataNascimento.getDate());
+            const diaParaOAniversario = calculaProximoAniversario(dataAtual, dataNascimento);
 
-            const diferencaMilissegundos = proximoAniversario.getTime() - dataAtual.getTime();
-            const diaParaOAniversario = Math.floor(diferencaMilissegundos / (1000 * 60 * 60 * 24));
-
-            console.log("Não foi dessa vez, mas seu próximo aniversário é daqui a ", diaParaOAniversario, " dias.");
-            res.innerHTML = `
-                <div class="container--mensagem aviso">
-                    <p>Não foi dessa vez, mas seu próximo aniversário é daqui a ${diaParaOAniversario} dias.</p>
-                </div>
-            `
+            msg = `Não foi dessa vez, mas seu próximo aniversário é daqui a ${diaParaOAniversario} dias.`;
+            res.innerHTML = exibirMensagem(msg, "aviso");
         }
     });
+
+    function exibirMensagem(msg, tipoMensagem) {
+        return `
+            <div class="container--mensagem ${tipoMensagem}">
+                <p>${msg}</p>
+            </div>
+        `;
+    }
+
+    function calculaProximoAniversario(dataAtual, dataNascimento) {
+        const proximoAniversario = new Date((dataAtual.getFullYear() + 1), dataNascimento.getMonth(), dataNascimento.getDate());
+        const diferencaMilissegundos = proximoAniversario.getTime() - dataAtual.getTime();
+
+        return Math.floor(diferencaMilissegundos / (1000 * 60 * 60 * 24));
+    }
 
 }); // Aqui fica o final do DOMContentLoaded
